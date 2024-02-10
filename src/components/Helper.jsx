b9273a4f12a080f3e29c7bf5db03ebd61b71ea74
Neha <neharadialcode@gmail.com>
2024-02-10 15:22:23 +0530

    pagination added

---
+++ src/components/Helper.jsx
@@ -0,0 +1,76 @@
+import profile from "../img/profile.png";
+
+export const card = [
+  {
+    profile: profile,
+    name: "ABC",
+    description: `    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere accusamus
+      aliquid repudiandae exercitationem distinctio, reiciendis totam quae
+      deserunt repellat amet commodi rem doloribus in libero natus voluptatibus
+      fuga, fugiat ad!`,
+  },
+  {
+    profile: profile,
+    name: "DEF",
+    description: `    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere accusamus
+      aliquid repudiandae exercitationem distinctio, reiciendis totam quae
+      deserunt repellat amet commodi rem doloribus in libero natus voluptatibus
+      fuga, fugiat ad!`,
+  },
+  {
+    profile: profile,
+    name: "GHI",
+    description: `    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere accusamus
+      aliquid repudiandae exercitationem distinctio, reiciendis totam quae
+      deserunt repellat amet commodi rem doloribus in libero natus voluptatibus
+      fuga, fugiat ad!`,
+  },
+  {
+    profile: profile,
+    name: "JKL",
+    description: `    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere accusamus
+      aliquid repudiandae exercitationem distinctio, reiciendis totam quae
+      deserunt repellat amet commodi rem doloribus in libero natus voluptatibus
+      fuga, fugiat ad!`,
+  },
+  {
+    profile: profile,
+    name: "MNO",
+    description: `    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere accusamus
+      aliquid repudiandae exercitationem distinctio, reiciendis totam quae
+      deserunt repellat amet commodi rem doloribus in libero natus voluptatibus
+      fuga, fugiat ad!`,
+  },
+  {
+    profile: profile,
+    name: "PRS",
+    description: `    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere accusamus
+      aliquid repudiandae exercitationem distinctio, reiciendis totam quae
+      deserunt repellat amet commodi rem doloribus in libero natus voluptatibus
+      fuga, fugiat ad!`,
+  },
+  {
+    profile: profile,
+    name: "TUV",
+    description: `    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere accusamus
+      aliquid repudiandae exercitationem distinctio, reiciendis totam quae
+      deserunt repellat amet commodi rem doloribus in libero natus voluptatibus
+      fuga, fugiat ad!`,
+  },
+  {
+    profile: profile,
+    name: "WXY",
+    description: `    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere accusamus
+      aliquid repudiandae exercitationem distinctio, reiciendis totam quae
+      deserunt repellat amet commodi rem doloribus in libero natus voluptatibus
+      fuga, fugiat ad!`,
+  },
+  {
+    profile: profile,
+    name: "ZZZ",
+    description: `    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere accusamus
+      aliquid repudiandae exercitationem distinctio, reiciendis totam quae
+      deserunt repellat amet commodi rem doloribus in libero natus voluptatibus
+      fuga, fugiat ad!`,
+  },
+];
--- src/components/pagination/StudentsCollection.jsx
+++ src/components/pagination/StudentsCollection.jsx
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import StudentCard from "./StudentCard";
+import { card } from "../Helper";
 
 const StudentsCollection = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 3;
+
+  const totalItems = card.length;
+  const totalPages = Math.ceil(totalItems / pageSize);
+
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+
+  const visibleStudents = card.slice(startIndex, endIndex + 1);
+
+  const changePage = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
   return (
     <div>
       <h2>Student data collection</h2>
-
-      <StudentCard />
+      <div className="flex gap-3">
+        {visibleStudents.map((obj, i) => (
+          <StudentCard key={i} obj={obj} i={i} />
+        ))}
+      </div>
+      {totalItems > pageSize && (
+        <div className="text-center mt-10">
+          <button
+            onClick={() => changePage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous Page
+          </button>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index + 1}
+              onClick={() => changePage(index + 1)}
+              className={
+                currentPage === index + 1 ? "bg-red-700 text-white" : ""
+              }
+            >
+              {index + 1}
+            </button>
+          ))}
+          <button
+            onClick={() => changePage(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Next Page
+          </button>
+        </div>
+      )}
     </div>
   );
 };
